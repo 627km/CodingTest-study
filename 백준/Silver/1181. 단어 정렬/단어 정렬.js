ea394d27c01576ee04ceb3fd84535d8a5534acd0
kyungmin Jeon <86594128+627km@users.noreply.github.com>
2024-01-19 14:41:50 +0900

    [Silver V] Title: 단어 정렬, Time: 236 ms, Memory: 18724 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -0,0 +1,32 @@
+let fs = require("fs");
+let input = fs.readFileSync("./dev/stdin").toString().split("\n");
+let n = Number(input[0]);
+
+let arr = [];
+for (let i = 1; i <= n; i++) {
+    arr.push(input[i]);
+}
+arr = [...new Set(arr)];
+
+function compare(a, b) {
+    if (a.length !== b.length) {
+        return a.length - b.length;
+    } else {
+        // 사전 순 정렬
+        if (a < b) {
+            return -1;
+        } else if (a > b) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
+
+arr.sort(compare);
+
+let answer = "";
+
+answer += arr.join("\n");
+
+console.log(answer);
