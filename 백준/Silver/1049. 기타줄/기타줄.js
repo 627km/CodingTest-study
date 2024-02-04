964412f20a03f5333a68730b6e677f2f66043855
kyungmin Jeon <86594128+627km@users.noreply.github.com>
2024-02-04 16:37:52 +0900

    [Silver IV] Title: 기타줄, Time: 128 ms, Memory: 9624 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.js
+++ 백준/Silver/1049. 기타줄/기타줄.js
@@ -1,32 +1,32 @@
 const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let input = fs
-    .readFileSync(filePath)
-    .toString()
-    .trim()
-    .split('\n')
-    .map((item) => item.split(' '));
+const input = fs.readFileSync('./dev/stdin').toString().split('\n');
 
-let [N, M] = input[0];
-let list = input.slice(1);
+let [N, brand] = input[0].split(' ').map(Number);
 
-let result = 0;
+let arr = [];
 
-// 세트와 낱개의 목록에서 최소가격을 구한다
-let minSet = Math.min(...list.map((item) => item[0]));
-let minPiece = Math.min(...list.map((item) => item[1]));
+let set = [];
+let piece = [];
 
-while (true) {
-    let set = Math.floor(N / 6); // 세트가 몇개 필요한지 구한다
-    if (N < 6) {
-        // 필요한 기타줄이 6개 미만일때
-        result += Math.min(minPiece * N, minSet); // 낱개와 세트로 사는것중에 저렴한걸 찾는다
-        break;
-    } else {
-        // 필요한 기타줄이 6개 이상일때
-        result += Math.min(minSet * set, minPiece * (set * 6)); // 낱개와 세트로 사는것중에 저렴한걸 찾는다
-        N -= 6 * set; // 구매한 줄의 갯수만큼 빼준다
-    }
+for (let i = 1; i <= brand; i++) {
+    set.push(input[i].split(' ').map(Number)[0]);
+    piece.push(input[i].split(' ').map(Number)[1]);
 }
 
-console.log(result);
+let minSet = Math.min(...set);
+let minPiece = Math.min(...piece);
+
+let flag = false;
+
+let answer = 0;
+while (!flag) {
+    let setNum = parseInt(N / 6);
+    if (N < 6) {
+        answer += Math.min(minSet, minPiece * N);
+        flag = true;
+    } else {
+        answer += Math.min(minSet * setNum, minPiece * setNum * 6);
+        N = N - setNum * 6;
+    }
+}
+console.log(answer);
