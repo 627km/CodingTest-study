bd757dc98e21804ea3b38e37f652da49f4ed54b3
kyungmin Jeon <86594128+627km@users.noreply.github.com>
2024-02-23 14:31:45 +0900

    [Silver II] Title: 병사 배치하기, Time: 180 ms, Memory: 10400 KB -BaekjoonHub

---
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.js
@@ -0,0 +1,29 @@
+let fs = require('fs');
+let input = fs.readFileSync('./dev/stdin').toString().split('\n');
+
+let n = Number(input[0]);
+let arr = input[1].split(' ').map(Number);
+
+arr.reverse();
+
+let d = [0];
+
+function lowerBound(arr, target, start, end) {
+  while (start < end) {
+    let mid = parseInt((start + end) / 2);
+    if (arr[mid] >= target) end = mid; // 최대한 왼쪽으로 이동하기
+    else start = mid + 1;
+  }
+  return end;
+}
+
+for (x of arr) {
+  if (d[d.length - 1] < x) {
+    d.push(x);
+  } else {
+    let index = lowerBound(d, x, 0, d.length);
+    d[index] = x;
+  }
+}
+
+console.log(n - (d.length - 1));
